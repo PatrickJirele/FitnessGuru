5c6ca4b6cd06ba95f6001da363a1dc77f69bfe5c
Patrick Jirele <pjjirele@gmail.com>
2024-04-27 19:31:47 -0600

    got ddb, login, and kogout working

--- main.py
+++ main.py
@@ -7,48 +7,62 @@ from flask_login import LoginManager, UserMixin, login_user, logout_user, curren
 app = Flask(__name__, static_url_path='/static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///FitnessGuruDB.db'
 app.config['SECRET_KEY'] = 'secretKey'
+login_manager = LoginManager(app)
 db = SQLAlchemy(app)
 
 class Benefit(db.Model):
+    __tablename__ = "benefit"
     Benefit_ID = db.Column(db.Integer, primary_key=True)
     Area = db.Column(db.String(50))
     Type = db.Column(db.String(50))
-    memberTier = db.relationship('Benefit_Tier', backref='Benefit')
+    memberTier = db.relationship('Benefit_Tier', backref='Benefit', lazy='dynamic')
 
 class Membership_Tier(db.Model):
-    Tier_ID = db.Column(db.String(50), primary_key=True)
+    __tablename__ = "membershiptier"
+    ID = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(50))
     Price = db.Column(db.Integer)
     Duration = db.Column(db.Integer)
-    benefit = db.relationship('Benefit_Tier', backref='Membership_Tier')
+    benefit = db.relationship('Benefit_Tier', backref='membershiptier', lazy='dynamic')
 
 class Benefit_Tier(db.Model):
-    Tier_ID = db.Column(db.Integer, db.ForeignKey('Membership_Tier.Tier_ID'), primary_key=True)
-    Benefit_ID = db.Column(db.Integer, db.ForeignKey('Benefit.Benefit_ID'), primary_key=True)
+    Tier_ID = db.Column(db.Integer, db.ForeignKey('membershiptier.ID'), primary_key=True)
+    Benefit_ID = db.Column(db.Integer, db.ForeignKey('benefit.Benefit_ID'), primary_key=True)
 
-class Member(db.Model):
+class Member(UserMixin, db.Model):
+    __tablename__ = "member"
     Member_ID = db.Column(db.Integer, primary_key=True)
     Fname = db.Column(db.String(50))
     Lname = db.Column(db.String(50))
-    Email = db.Column(db.String(50))
     DOB = db.Column(db.String(50))
-    Tier_ID = db.Column(db.String(50), db.ForeignKey('Membership_Tier.Tier_ID'))
-    Trainer_ID = db.Column(db.Integer, db.ForeignKey('Trainer.Trainer_ID'))
+    Email = db.Column(db.String(50))
+    Password = db.Column(db.String(50))
+    Tier_ID = db.Column(db.String(50), db.ForeignKey('membershiptier.ID'))
+    Trainer_ID = db.Column(db.Integer, db.ForeignKey('trainer.Trainer_ID'))
+
+    def get_id(self):
+        return str(self.Member_ID)
+
 
 class Trainer(db.Model):
+    __tablename__ = "trainer"
     Trainer_ID = db.Column(db.Integer, primary_key=True)
     Fname = db.Column(db.String(50))
     Lname = db.Column(db.String(50))
-    Member_ID = db.relationship('Member', backref='Trainer')
+    members = db.relationship('Member', backref='trainer', lazy='dynamic')
+
 
 class ClassXMember(db.Model):
-    Member_ID = db.Column(db.Integer, db.ForeignKey('Member.Member_ID'), primary_key=True)
-    Class_ID = db.Column(db.Integer, db.ForeignKey('Class.Class_ID'), primary_key=True)
+    __tablename__ = 'classxmember'
+    Member_ID = db.Column(db.Integer, db.ForeignKey('member.Member_ID'), primary_key=True)
+    Class_ID = db.Column(db.Integer, db.ForeignKey('class.Class_ID'), primary_key=True)
     start_date = db.Column(db.String(50))
     end_date = db.Column(db.String(50))
 
 class Class(db.Model):
+    __tablename__ = 'class'
     Class_ID = db.Column(db.Integer, primary_key=True)
-    classXmember_ID = db.relationship('ClassXMember', backref='Class')
+    classXmembers = db.relationship('ClassXMember', backref='class', lazy='dynamic')
     Instructor = db.Column(db.String(50))
     days_of_week = db.Column(db.String(50))
     Time = db.Column(db.String(50))
@@ -56,17 +70,63 @@ class Class(db.Model):
     Max_capacity = db.Column(db.Integer)
     Enrollment = db.Column(db.Integer)
 
+
 class ExerciseXMember(db.Model):
-    Member_ID = db.Column(db.Integer, db.ForeignKey('Member.Member_ID'), primary_key=True)
-    Exercise_ID = db.Column(db.Integer, db.ForeignKey('Exercise.Exercise_ID'), primary_key=True)
+    __tablename__ = 'exercisexmember'
+    Member_ID = db.Column(db.Integer, db.ForeignKey('member.Member_ID'), primary_key=True)
+    Exercise_ID = db.Column(db.Integer, db.ForeignKey('exercise.Exercise_ID'), primary_key=True)
     days_of_week = db.Column(db.String(50))
     start_date = db.Column(db.String(50))
     end_date = db.Column(db.String(50))
 
 class Exercise(db.Model):
+    __tablename__ = 'exercise'
     Exercise_ID = db.Column(db.Integer, primary_key=True)
-    exerciseXmember_ID = db.relationship('ExerciseXMember', backref='Exercise')
+    exerciseXmembers = db.relationship('ExerciseXMember', backref='exercise', lazy='dynamic')
     Type = db.Column(db.String(50))
     Reps = db.Column(db.Integer)
     Weight = db.Column(db.Double)
     Sets = db.Column(db.Integer)
+
+@app.route('/')
+def home():
+    login = False
+    return render_template('home.html', login=login)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        Email = request.form['username']
+        Password = request.form['password']
+
+        user = Member.query.filter_by(Email=Email).first()
+
+        if Email != user.Email or Password != user.Password:
+            uniqueName = False
+            return render_template('login.html', uniqueName=uniqueName)
+
+        login_user(user)
+        login = True
+        return render_template('home.html', login=login)
+
+    elif request.method == 'GET':
+        uniqueName = True
+        return render_template('login.html')
+
+app.app_context().push()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Member.query.get(int(user_id))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    login=False
+    return render_template('home.html', login=login)
+
+
+
+if __name__ == '__main__':
+    app.run()
